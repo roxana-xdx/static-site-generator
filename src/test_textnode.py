84c1014a89cd2b1d0926ffec9270cc3f71076f9f
roxana-xdx <142057783+roxana-xdx@users.noreply.github.com>
2026-02-24 16:11:03 +0200

    added TextNode to HTMLNode function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -27,6 +27,27 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(Node, text, https://www.boot.dev)", repr(node)
         )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_link(self):
+        node = TextNode("Link description", TextType.LINK, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Link description")
+        self.assertEqual(html_node.props, {"href": "www.google.com"})
+
+    def test_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "www.google.com", "alt": "Alt text"})
 
 if __name__ == "__main__":
     unitttest.main()
