40fa1b83e6be2cbe28c61a5ce7309b5782c0bea4
roxana-xdx <142057783+roxana-xdx@users.noreply.github.com>
2026-02-27 20:35:03 +0200

    added block to BlockType function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     block_strings = markdown.split("\n\n")
     new_blocks = []
@@ -6,3 +17,32 @@ def markdown_to_blocks(markdown):
         if new_string != "":
             new_blocks.append(new_string)
     return new_blocks
+
+def block_to_block_type(markdown):
+    lines = re.findall(r"^#{1,6} .+", markdown)
+    if len(lines) != 0:
+        return BlockType.HEADING
+    
+    lines = re.findall(r"^```\n.+\n```", markdown)
+    if len(lines) != 0:
+        return BlockType.CODE
+    
+    lines = re.findall(r"(^> ?.+\n)+", markdown)
+    if len(lines) != 0:
+        return BlockType.QUOTE
+    
+    lines = re.findall(r"(^- .+)+", markdown)
+    if len(lines) != 0:
+        return BlockType.UNORDERED_LIST
+    
+    lines = re.findall(r"(^\d+\. .+)+", markdown)
+    if len(lines) != 0:
+        ordered = True
+        for i in range(len(lines)):
+            if not lines[i].startswith(f"{i+1}. "):
+                ordered = False
+        if ordered:
+            return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -42,3 +42,59 @@ This is another paragraph
                 "This is another paragraph",
             ],
         )
+
+    def test_block_to_block_type_heading(self):
+        md = "# Heading 1"
+        self.assertEqual(block_to_block_type(md), BlockType.HEADING)
+
+        md = "###### Heading 6 example"
+        self.assertEqual(block_to_block_type(md), BlockType.HEADING)
+
+        md = "#This is not a Heading"
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_multiline_code(self):
+        md = """```
+Multiline code example
+```"""
+        self.assertEqual(block_to_block_type(md), BlockType.CODE)
+
+        md = """```
+```"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+        md = """```
+This isn't a proper code block
+``"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_quote(self):
+        md = """> This is a quote!
+>This is the second part of the quote."""
+        self.assertEqual(block_to_block_type(md), BlockType.QUOTE)
+
+    def test_block_to_block_unordered_list(self):
+        md = """- This is
+- a valid
+- unordered list."""
+        self.assertEqual(block_to_block_type(md), BlockType.UNORDERED_LIST)
+
+        md = """-This is NOT
+-a valid list."""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+    def test_block_to_block_ordered_list(self):
+        md = "1. This is an ordered list."
+        self.assertEqual(block_to_block_type(md), BlockType.ORDERED_LIST)
+
+        md = """1. This is another ordered list
+2. With 2 items!"""
+        self.assertEqual(block_to_block_type(md), BlockType.ORDERED_LIST)
+
+        md = """1.This is not a valid ordered list
+2. Spaces are (sometimes) important in markdown"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
+
+        md = """2. This is not a valid ordered list
+1. Order matters!"""
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
