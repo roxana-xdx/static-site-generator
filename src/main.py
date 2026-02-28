25f7fed2057ba15d4e2d80196e5be495b4cbf697
roxana-xdx <142057783+roxana-xdx@users.noreply.github.com>
2026-02-28 15:53:41 +0200

    called generate_page in main.py and hosted page locally with main.sh

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
+import os
 from copy_static import copy_files
+from generate_page import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     
     copy_files(dir_path_static, dir_path_public)
-    print("Successfully copied static files to public directory.")
+    print("Copied static files to public directory.")
+
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html")
+    )
+    print("Page generated successfully.")
 
 if __name__ == "__main__":
     main()
